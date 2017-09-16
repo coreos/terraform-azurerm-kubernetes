2865656e9230f1e4110bfcefec8cc35f5b20d801
Lucas Serven <lserven@gmail.com>
2017-09-16 15:36:22 -0700

    1.7.3-tectonic.2

---
+++ main.tf
@@ -0,0 +1,180 @@
+provider "azurerm" {
+  environment   = "${var.tectonic_azure_cloud_environment}"
+  client_secret = "${var.tectonic_azure_client_secret}"
+}
+
+data "azurerm_client_config" "current" {}
+
+module "resource_group" {
+  source = "github.com/coreos/tectonic-installer//modules/azure/resource-group?ref=1.7.3-tectonic.2"
+
+  external_rsg_id = "${var.tectonic_azure_external_resource_group}"
+  azure_location  = "${var.tectonic_azure_location}"
+  cluster_name    = "${var.tectonic_cluster_name}"
+  cluster_id      = "${module.tectonic.cluster_id}"
+  extra_tags      = "${var.tectonic_azure_extra_tags}"
+}
+
+module "vnet" {
+  source = "github.com/coreos/tectonic-installer//modules/azure/vnet?ref=1.7.3-tectonic.2"
+
+  location            = "${var.tectonic_azure_location}"
+  resource_group_name = "${module.resource_group.name}"
+  cluster_name        = "${var.tectonic_cluster_name}"
+  cluster_id          = "${module.tectonic.cluster_id}"
+  base_domain         = "${var.tectonic_base_domain}"
+  vnet_cidr_block     = "${var.tectonic_azure_vnet_cidr_block}"
+
+  etcd_count           = "${var.tectonic_experimental ? 0 : max(var.tectonic_etcd_count, 1)}"
+  master_count         = "${var.tectonic_master_count}"
+  worker_count         = "${var.tectonic_worker_count}"
+  etcd_cidr            = "${module.vnet.etcd_cidr}"
+  master_cidr          = "${module.vnet.master_cidr}"
+  worker_cidr          = "${module.vnet.worker_cidr}"
+  ssh_network_internal = "${var.tectonic_azure_ssh_network_internal}"
+  ssh_network_external = "${var.tectonic_azure_ssh_network_external}"
+
+  external_vnet_id          = "${var.tectonic_azure_external_vnet_id}"
+  external_master_subnet_id = "${var.tectonic_azure_external_master_subnet_id}"
+  external_worker_subnet_id = "${var.tectonic_azure_external_worker_subnet_id}"
+  external_nsg_etcd_id      = "${var.tectonic_azure_external_nsg_etcd_id}"
+  external_nsg_api_id       = "${var.tectonic_azure_external_nsg_api_id}"
+  external_nsg_master_id    = "${var.tectonic_azure_external_nsg_master_id}"
+  external_nsg_worker_id    = "${var.tectonic_azure_external_nsg_worker_id}"
+
+  extra_tags = "${var.tectonic_azure_extra_tags}"
+}
+
+module "etcd" {
+  source = "github.com/coreos/tectonic-installer//modules/azure/etcd?ref=1.7.3-tectonic.2"
+
+  location            = "${var.tectonic_azure_location}"
+  resource_group_name = "${module.resource_group.name}"
+  vm_size             = "${var.tectonic_azure_etcd_vm_size}"
+  storage_type        = "${var.tectonic_azure_etcd_storage_type}"
+  storage_id          = "${module.resource_group.storage_id}"
+  container_image     = "${var.tectonic_container_images["etcd"]}"
+
+  etcd_count            = "${var.tectonic_experimental ? 0 : max(var.tectonic_etcd_count, 1)}"
+  base_domain           = "${var.tectonic_base_domain}"
+  cluster_name          = "${var.tectonic_cluster_name}"
+  cluster_id            = "${module.tectonic.cluster_id}"
+  public_ssh_key        = "${var.tectonic_azure_ssh_key}"
+  network_interface_ids = "${module.vnet.etcd_network_interface_ids}"
+  versions              = "${var.tectonic_versions}"
+  cl_channel            = "${var.tectonic_cl_channel}"
+
+  tls_enabled        = "${var.tectonic_etcd_tls_enabled}"
+  tls_ca_crt_pem     = "${module.bootkube.etcd_ca_crt_pem}"
+  tls_server_crt_pem = "${module.bootkube.etcd_server_crt_pem}"
+  tls_server_key_pem = "${module.bootkube.etcd_server_key_pem}"
+  tls_client_crt_pem = "${module.bootkube.etcd_client_crt_pem}"
+  tls_client_key_pem = "${module.bootkube.etcd_client_key_pem}"
+  tls_peer_crt_pem   = "${module.bootkube.etcd_peer_crt_pem}"
+  tls_peer_key_pem   = "${module.bootkube.etcd_peer_key_pem}"
+
+  extra_tags = "${var.tectonic_azure_extra_tags}"
+}
+
+# Workaround for https://github.com/hashicorp/terraform/issues/4084
+data "null_data_source" "cloud_provider" {
+  inputs = {
+    "cloud"                      = "${var.tectonic_azure_cloud_environment}"
+    "tenantId"                   = "${data.azurerm_client_config.current.tenant_id}"
+    "subscriptionId"             = "${data.azurerm_client_config.current.subscription_id}"
+    "aadClientId"                = "${data.azurerm_client_config.current.client_id}"
+    "aadClientSecret"            = "${var.tectonic_azure_client_secret}"
+    "resourceGroup"              = "${module.resource_group.name}"
+    "location"                   = "${var.tectonic_azure_location}"
+    "subnetName"                 = "${module.vnet.worker_subnet_name}"
+    "securityGroupName"          = "${module.vnet.worker_nsg_name}"
+    "vnetName"                   = "${module.vnet.vnet_id}"
+    "primaryAvailabilitySetName" = "${module.workers.availability_set_name}"
+  }
+}
+
+module "masters" {
+  source = "github.com/coreos/tectonic-installer//modules/azure/master-as?ref=1.7.3-tectonic.2"
+
+  location            = "${var.tectonic_azure_location}"
+  resource_group_name = "${module.resource_group.name}"
+  vm_size             = "${var.tectonic_azure_master_vm_size}"
+  storage_type        = "${var.tectonic_azure_master_storage_type}"
+  storage_id          = "${module.resource_group.storage_id}"
+
+  master_count                 = "${var.tectonic_master_count}"
+  base_domain                  = "${var.tectonic_base_domain}"
+  cluster_name                 = "${var.tectonic_cluster_name}"
+  cluster_id                   = "${module.tectonic.cluster_id}"
+  public_ssh_key               = "${var.tectonic_azure_ssh_key}"
+  virtual_network              = "${module.vnet.vnet_id}"
+  network_interface_ids        = "${module.vnet.master_network_interface_ids}"
+  kube_image_url               = "${replace(var.tectonic_container_images["hyperkube"],var.tectonic_image_re,"$1")}"
+  kube_image_tag               = "${replace(var.tectonic_container_images["hyperkube"],var.tectonic_image_re,"$2")}"
+  kubeconfig_content           = "${module.bootkube.kubeconfig}"
+  tectonic_kube_dns_service_ip = "${module.bootkube.kube_dns_service_ip}"
+  cloud_provider               = "azure"
+  cloud_provider_config        = "${jsonencode(data.null_data_source.cloud_provider.inputs)}"
+  kubelet_node_label           = "node-role.kubernetes.io/master"
+  kubelet_node_taints          = "node-role.kubernetes.io/master=:NoSchedule"
+  kubelet_cni_bin_dir          = "${var.tectonic_calico_network_policy ? "/var/lib/cni/bin" : "" }"
+  bootkube_service             = "${module.bootkube.systemd_service}"
+  tectonic_service             = "${module.tectonic.systemd_service}"
+  tectonic_service_disabled    = "${var.tectonic_vanilla_k8s}"
+  versions                     = "${var.tectonic_versions}"
+  cl_channel                   = "${var.tectonic_cl_channel}"
+
+  extra_tags = "${var.tectonic_azure_extra_tags}"
+}
+
+module "workers" {
+  source = "github.com/coreos/tectonic-installer//modules/azure/worker-as?ref=1.7.3-tectonic.2"
+
+  location            = "${var.tectonic_azure_location}"
+  resource_group_name = "${module.resource_group.name}"
+  vm_size             = "${var.tectonic_azure_worker_vm_size}"
+  storage_type        = "${var.tectonic_azure_worker_storage_type}"
+  storage_id          = "${module.resource_group.storage_id}"
+
+  worker_count                 = "${var.tectonic_worker_count}"
+  cluster_name                 = "${var.tectonic_cluster_name}"
+  cluster_id                   = "${module.tectonic.cluster_id}"
+  public_ssh_key               = "${var.tectonic_azure_ssh_key}"
+  virtual_network              = "${module.vnet.vnet_id}"
+  network_interface_ids        = "${module.vnet.worker_network_interface_ids}"
+  kube_image_url               = "${replace(var.tectonic_container_images["hyperkube"],var.tectonic_image_re,"$1")}"
+  kube_image_tag               = "${replace(var.tectonic_container_images["hyperkube"],var.tectonic_image_re,"$2")}"
+  kubeconfig_content           = "${module.bootkube.kubeconfig}"
+  tectonic_kube_dns_service_ip = "${module.bootkube.kube_dns_service_ip}"
+  cloud_provider               = "azure"
+  cloud_provider_config        = "${jsonencode(data.null_data_source.cloud_provider.inputs)}"
+  kubelet_node_label           = "node-role.kubernetes.io/node"
+  kubelet_cni_bin_dir          = "${var.tectonic_calico_network_policy ? "/var/lib/cni/bin" : "" }"
+  versions                     = "${var.tectonic_versions}"
+  cl_channel                   = "${var.tectonic_cl_channel}"
+
+  extra_tags = "${var.tectonic_azure_extra_tags}"
+}
+
+module "dns" {
+  source = "github.com/coreos/tectonic-installer//modules/dns/azure?ref=1.7.3-tectonic.2"
+
+  etcd_count   = "${var.tectonic_experimental ? 0 : var.tectonic_etcd_count}"
+  master_count = "${var.tectonic_master_count}"
+  worker_count = "${var.tectonic_worker_count}"
+
+  etcd_ip_addresses    = "${module.vnet.etcd_endpoints}"
+  master_ip_addresses  = "${module.vnet.master_private_ip_addresses}"
+  worker_ip_addresses  = "${module.vnet.worker_private_ip_addresses}"
+  api_ip_addresses     = "${module.vnet.api_ip_addresses}"
+  console_ip_addresses = "${module.vnet.console_ip_addresses}"
+
+  base_domain  = "${var.tectonic_base_domain}"
+  cluster_name = "${var.tectonic_cluster_name}"
+  cluster_id   = "${module.tectonic.cluster_id}"
+
+  location             = "${var.tectonic_azure_location}"
+  external_dns_zone_id = "${var.tectonic_azure_external_dns_zone_id}"
+
+  extra_tags = "${var.tectonic_azure_extra_tags}"
+}
---
+++ tectonic.tf
@@ -0,0 +1,128 @@
+module "bootkube" {
+  source = "github.com/coreos/tectonic-installer//modules/bootkube?ref=1.7.3-tectonic.2"
+
+  cloud_provider        = "azure"
+  cloud_provider_config = "${jsonencode(data.null_data_source.cloud_provider.inputs)}"
+
+  cluster_name = "${var.tectonic_cluster_name}"
+
+  kube_apiserver_url = "https://${module.vnet.api_fqdn}:443"
+  oidc_issuer_url    = "https://${module.vnet.ingress_fqdn}/identity"
+
+  # Platform-independent variables wiring, do not modify.
+  container_images = "${var.tectonic_container_images}"
+  versions         = "${var.tectonic_versions}"
+
+  ca_cert    = "${var.tectonic_ca_cert}"
+  ca_key     = "${var.tectonic_ca_key}"
+  ca_key_alg = "${var.tectonic_ca_key_alg}"
+
+  service_cidr = "${var.tectonic_service_cidr}"
+  cluster_cidr = "${var.tectonic_cluster_cidr}"
+
+  advertise_address = "0.0.0.0"
+  anonymous_auth    = "false"
+
+  oidc_username_claim = "email"
+  oidc_groups_claim   = "groups"
+  oidc_client_id      = "tectonic-kubectl"
+
+  etcd_endpoints      = "${module.etcd.node_names}"
+  etcd_cert_dns_names = "${module.etcd.node_names}"
+  etcd_ca_cert        = "${var.tectonic_etcd_ca_cert_path}"
+  etcd_client_cert    = "${var.tectonic_etcd_client_cert_path}"
+  etcd_client_key     = "${var.tectonic_etcd_client_key_path}"
+  etcd_tls_enabled    = "${var.tectonic_etcd_tls_enabled}"
+
+  experimental_enabled = "${var.tectonic_experimental}"
+
+  master_count = "${var.tectonic_master_count}"
+}
+
+module "tectonic" {
+  source   = "github.com/coreos/tectonic-installer//modules/tectonic?ref=1.7.3-tectonic.2"
+  platform = "azure"
+
+  cluster_name = "${var.tectonic_cluster_name}"
+
+  base_address       = "${module.vnet.ingress_fqdn}"
+  kube_apiserver_url = "https://${module.vnet.api_fqdn}:443"
+
+  # Platform-independent variables wiring, do not modify.
+  container_images = "${var.tectonic_container_images}"
+  versions         = "${var.tectonic_versions}"
+
+  license_path     = "${var.tectonic_vanilla_k8s ? "/dev/null" : pathexpand(var.tectonic_license_path)}"
+  pull_secret_path = "${var.tectonic_vanilla_k8s ? "/dev/null" : pathexpand(var.tectonic_pull_secret_path)}"
+
+  admin_email         = "${var.tectonic_admin_email}"
+  admin_password_hash = "${var.tectonic_admin_password_hash}"
+
+  update_channel = "${var.tectonic_update_channel}"
+  update_app_id  = "${var.tectonic_update_app_id}"
+  update_server  = "${var.tectonic_update_server}"
+
+  ca_generated = "${module.bootkube.ca_cert == "" ? false : true}"
+  ca_cert      = "${module.bootkube.ca_cert}"
+  ca_key_alg   = "${module.bootkube.ca_key_alg}"
+  ca_key       = "${module.bootkube.ca_key}"
+
+  console_client_id = "tectonic-console"
+  kubectl_client_id = "tectonic-kubectl"
+  ingress_kind      = "NodePort"
+  experimental      = "${var.tectonic_experimental}"
+  master_count      = "${var.tectonic_master_count}"
+  stats_url         = "${var.tectonic_stats_url}"
+
+  image_re = "${var.tectonic_image_re}"
+}
+
+module "flannel-vxlan" {
+  source = "github.com/coreos/tectonic-installer//modules/net/flannel-vxlan?ref=1.7.3-tectonic.2"
+
+  flannel_image     = "${var.tectonic_container_images["flannel"]}"
+  flannel_cni_image = "${var.tectonic_container_images["flannel_cni"]}"
+  cluster_cidr      = "${var.tectonic_cluster_cidr}"
+
+  bootkube_id = "${module.bootkube.id}"
+}
+
+module "calico-network-policy" {
+  source = "github.com/coreos/tectonic-installer//modules/net/calico-network-policy?ref=1.7.3-tectonic.2"
+
+  kube_apiserver_url = "https://${module.vnet.api_fqdn}:443"
+  calico_image       = "${var.tectonic_container_images["calico"]}"
+  calico_cni_image   = "${var.tectonic_container_images["calico_cni"]}"
+  cluster_cidr       = "${var.tectonic_cluster_cidr}"
+  enabled            = "${var.tectonic_calico_network_policy}"
+
+  bootkube_id = "${module.bootkube.id}"
+}
+
+resource "null_resource" "tectonic" {
+  depends_on = ["module.vnet", "module.dns", "module.etcd", "module.masters", "module.bootkube", "module.tectonic", "module.flannel-vxlan", "module.calico-network-policy"]
+
+  triggers {
+    api-endpoint = "${module.vnet.api_fqdn}"
+  }
+
+  connection {
+    host  = "${module.vnet.api_fqdn}"
+    user  = "core"
+    agent = true
+  }
+
+  provisioner "file" {
+    source      = "./generated"
+    destination = "$HOME/tectonic"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo mkdir -p /opt",
+      "sudo rm -rf /opt/tectonic",
+      "sudo mv /home/core/tectonic /opt/",
+      "sudo systemctl start ${var.tectonic_vanilla_k8s ? "bootkube.service" : "tectonic.service"}",
+    ]
+  }
+}
